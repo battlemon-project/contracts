3017c356653de932fa135e13fbd0979ab830b23c
fomotoshi <fomotoshi@ya.ru>
2021-11-01 19:14:47 +0300

    added shell scripts for routines.

---
+++ market_build_and_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+MAIN_CONTRACT="battlemon.testnet"
+MARKET_CONTRACT="market.${MAIN_CONTRACT}"
+NFT_CONTRACT="nft.${MAIN_CONTRACT}"
+
+cargo build -p nft_market --target wasm32-unknown-unknown --release
+
+near delete $MARKET_CONTRACT $MAIN_CONTRACT
+near create-account $MARKET_CONTRACT --masterAccount $MAIN_CONTRACT
+near deploy $MARKET_CONTRACT ./target/wasm32-unknown-unknown/release/nft_market.wasm --masterAccount $MAIN_CONTRACT --initDeposit 50
+near call $MARKET_CONTRACT init '{"nft_id": "'$NFT_CONTRACT'"}' --accountId $MAIN_CONTRACT
+
+near call nft.battlemon.testnet nft_approve '{"token_id": "2", "account_id": "market.battlemon.testnet", "msg": "{\"price\":\"2\"}"}' --accountId nft.battlemon.testnet --deposit 1
+near call market.battlemon.testnet buy '{"token_id": "2"}' --deposit 3 --accountId fomotosh1.testnet
