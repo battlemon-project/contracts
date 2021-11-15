903406401792df77c51a9cbcf3e265e07913888d
fomotoshi <fomotoshi@ya.ru>
2021-11-15 19:20:17 +0300

    fixed ci and deploy scripts

--- dev_nft_deploy.sh
+++ dev_nft_deploy.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-near dev-deploy ./target/wasm32-unknown-unknown/release/nft_token.wasm -f
+echo n | near dev-deploy ./target/wasm32-unknown-unknown/release/nft_token.wasm
 
 source ./neardev/dev-account.env
+export MARKET=market.$CONTRACT_NAME
 
 near call $CONTRACT_NAME init '{"owner_id": "'$CONTRACT_NAME'"}' --accountId $CONTRACT_NAME
 
@@ -12,3 +13,7 @@ for i in {1..10}
 do
   near call $CONTRACT_NAME  mint '{"token_id": "'$i'", "token_metadata": {"title": "Title for token '$i'", "description": "some description for battlemon nft token", "media": "http://some-link-to-media.com", "properties": {"option": "on_sale", "century": "our_time", "type": "light", "lemon_gen": "nakamoto", "background": "red", "top": "headdress", "cyber_suit": "metallic", "expression": "brooding", "eyes": "open", "hair": "bob_marley", "accessory": "cigar", "winrate": 14, "rarity": 12}}}' --accountId $CONTRACT_NAME --amount 0.1
 done
+
+near create-account $MARKET --masterAccount $CONTRACT_NAME
+near deploy $MARKET ./target/wasm32-unknown-unknown/release/nft_market.wasm --masterAccount $CONTRACT_NAME --initDeposit 50
+near call $MARKET init '{"nft_id": "'$CONTRACT_NAME'"}' --accountId $CONTRACT_NAME
