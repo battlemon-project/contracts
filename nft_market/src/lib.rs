2cf1256220dba8ca4fefc76014a0147c9817efd5
fomotoshi <fomotoshi@ya.ru>
2021-11-16 01:11:03 +0300

    base implementation trade history was added

--- nft_market/src/lib.rs
+++ nft_market/src/lib.rs
@@ -9,8 +9,9 @@ use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::serde_json::{self, json};
 use near_sdk::{
     env, log, near_bindgen, require, AccountId, Balance, BorshStorageKey, Gas, PanicOnDefault,
-    Promise, PromiseError, PromiseOrValue, PromiseResult,
+    Promise, PromiseError, PromiseOrValue, PromiseResult, Timestamp,
 };
+use std::borrow::BorrowMut;
 
 pub const NO_DEPOSIT: Balance = 0;
 pub const ONE_YOCTO: Balance = 1;
@@ -24,9 +25,39 @@ pub struct Contract {
     nft_id: AccountId,
     asks: UnorderedMap<TokenId, SaleCondition>,
     bids: UnorderedMap<TokenId, Vector<U128>>,
+    trade_history: UnorderedMap<TokenId, Vector<Trade>>,
 }
 
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[derive(BorshSerialize, BorshDeserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub enum TradeType {
+    Sell,
+    Buy,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Trade {
+    pub prev_owner: AccountId,
+    pub curr_owner: AccountId,
+    pub price: U128,
+    pub date: Timestamp,
+    pub type_: TradeType,
+}
+
+impl Trade {
+    pub fn from_sale(sale: SaleCondition, curr_owner: AccountId, type_: TradeType) -> Self {
+        Self {
+            prev_owner: sale.owner_id,
+            curr_owner,
+            price: sale.price,
+            date: env::block_timestamp(),
+            type_,
+        }
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
 pub struct SaleCondition {
     pub owner_id: AccountId,
@@ -56,6 +87,8 @@ pub struct SaleArgs {
 enum StorageKey {
     Asks,
     Bids,
+    Orders,
+    TradeHistory,
 }
 
 #[near_sdk::ext_contract]
@@ -71,7 +104,7 @@ trait ExtNft {
 
 #[near_sdk::ext_contract(ext_self)]
 trait ExtSelf {
-    fn after_nft_transfer(&mut self, sale: SaleCondition);
+    fn after_nft_transfer(&mut self, sale: SaleCondition, buyer_id: AccountId);
 }
 
 #[near_bindgen]
@@ -82,6 +115,7 @@ impl Contract {
             nft_id,
             asks: UnorderedMap::new(StorageKey::Asks),
             bids: UnorderedMap::new(StorageKey::Bids),
+            trade_history: UnorderedMap::new(StorageKey::TradeHistory),
         }
     }
 
@@ -115,7 +149,7 @@ impl Contract {
         let sale = self.asks.get(&token_id).unwrap();
 
         ext_nft::nft_transfer(
-            buyer_id,
+            buyer_id.clone(),
             token_id,
             Some(sale.approval_id),
             None,
@@ -125,6 +159,7 @@ impl Contract {
         )
         .then(ext_self::after_nft_transfer(
             sale,
+            buyer_id,
             env::current_account_id(),
             NO_DEPOSIT,
             AFTER_NFT_TRANSFER_GAS,
@@ -132,15 +167,32 @@ impl Contract {
     }
 
     #[private]
-    pub fn after_nft_transfer(&mut self, sale: SaleCondition) {
+    pub fn after_nft_transfer(&mut self, sale: SaleCondition, buyer_id: AccountId) {
         match env::promise_result(0) {
             PromiseResult::Successful(_) => {
                 self.asks.remove(&sale.token_id);
-                Promise::new(env::current_account_id()).transfer(sale.price.0);
+                self.add_trade_history(sale.clone(), buyer_id);
+                Promise::new(sale.owner_id).transfer(sale.price.0);
             }
             PromiseResult::Failed => panic_str("Execution `nft_transfer` method was failed."),
             PromiseResult::NotReady => unreachable!(),
         }
+    }
+
+    fn add_trade_history(&mut self, sale: SaleCondition, buyer_id: AccountId) {
+        let trade = Trade::from_sale(sale.clone(), buyer_id, TradeType::Sell);
+        let mut history = self
+            .trade_history
+            .get(&sale.token_id)
+            .unwrap_or_else(|| Vector::new(StorageKey::Orders));
+        history.push(&trade);
+        self.trade_history.insert(&sale.token_id, &history);
+    }
+
+    pub fn list_trade_history_by_token_id(&self, token_id: TokenId) -> Vec<Trade> {
+        self.trade_history
+            .get(&token_id)
+            .map_or(Vec::new(), |v| v.to_vec())
     }
 }
 
