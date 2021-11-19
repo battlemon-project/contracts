6cef847d147f306c4524a04b506d00e177b02c7f
fomotoshi <fomotoshi@ya.ru>
2021-11-19 18:46:47 +0300

    play2earn was implemented

--- play2earn/src/lib.rs
+++ play2earn/src/lib.rs
@@ -2,17 +2,17 @@
 //! It's accumulates player's match statistics and send Juice fungible token as royalties for
 //! the player's progress.
 
-use near_contract_standards::non_fungible_token::TokenId;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::env::panic_str;
 use near_sdk::json_types::U128;
 use near_sdk::serde::{self, Deserialize, Serialize};
 use near_sdk::{
-    assert_one_yocto, env, ext_contract, near_bindgen, require, AccountId, Balance,
-    BorshStorageKey, Gas, PanicOnDefault, Promise,
+    env, ext_contract, near_bindgen, require, AccountId, Balance, BorshStorageKey, Gas,
+    PanicOnDefault, Promise, PromiseResult,
 };
 
-const PROCESS_PROGRESS_GAS: Gas = Gas(10_000_000_000_000 + FT_TRANSFER_GAS.0);
-const FT_TRANSFER_GAS: Gas = Gas(5_000_000_000_000);
+const PROCESS_PROGRESS_GAS: Gas = Gas(50_000_000_000_000 + FT_TRANSFER_GAS.0);
+const FT_TRANSFER_GAS: Gas = Gas(20_000_000_000_000);
 const ONE_YOCTO: Balance = 1;
 
 ///Juice's interface of smart-contract
@@ -24,6 +24,17 @@ trait ExtJuice {
         amount: U128,
         memo: Option<String>,
     ) -> Promise;
+
+    fn storage_deposit(
+        &mut self,
+        account_id: Option<AccountId>,
+        registration_only: Option<bool>,
+    ) -> Promise;
+}
+
+#[ext_contract(ext_self)]
+trait ExtSelf {
+    fn after_storage_deposit(&mut self, player_id: AccountId, royalty_amount: U128) -> Promise;
 }
 
 #[derive(BorshSerialize, BorshStorageKey)]
@@ -81,8 +92,9 @@ impl Contract {
     /// Deserialize JSON into `Progress`, calculate juice and give out royalties
     /// # Arguments
     /// * `progress` - `Progress` deserialized struct.
+    #[payable]
     pub fn process_progress(&mut self, progress: Progress) -> Promise {
-        assert_one_yocto();
+        // assert_one_yocto();
         require!(
             env::prepaid_gas() >= PROCESS_PROGRESS_GAS,
             format!("prepared gas must be more than {}", PROCESS_PROGRESS_GAS.0)
@@ -93,13 +105,36 @@ impl Contract {
         );
 
         let royalty_amount = progress.calculate_juice();
-        ext_juice::ft_transfer(
-            progress.player_id,
-            U128(royalty_amount as u128),
+        ext_juice::storage_deposit(
+            Some(progress.player_id.clone()),
             None,
             self.juice_id.clone(),
-            ONE_YOCTO,
-            FT_TRANSFER_GAS,
+            env::attached_deposit(),
+            PROCESS_PROGRESS_GAS,
         )
+        .then(ext_self::after_storage_deposit(
+            progress.player_id,
+            U128(royalty_amount as u128),
+            env::current_account_id(),
+            ONE_YOCTO,
+            FT_TRANSFER_GAS * 3,
+        ))
+    }
+
+    #[private]
+    #[payable]
+    pub fn after_storage_deposit(&mut self, player_id: AccountId, royalty_amount: U128) -> Promise {
+        match env::promise_result(0) {
+            PromiseResult::NotReady => unreachable!(),
+            PromiseResult::Successful(_) => ext_juice::ft_transfer(
+                player_id,
+                royalty_amount,
+                Some("Mmm yammy juice".to_string()),
+                self.juice_id.clone(),
+                ONE_YOCTO,
+                FT_TRANSFER_GAS,
+            ),
+            PromiseResult::Failed => panic_str("Storage deposit was failed."),
+        }
     }
 }
