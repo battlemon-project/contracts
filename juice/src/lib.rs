9b2e934f5e19b170184ac2a84ba78df0a2d2b368
fomotoshi <fomotoshi@ya.ru>
2021-11-19 18:46:12 +0300

    ft juice was implemented

---
+++ juice/src/lib.rs
@@ -0,0 +1,67 @@
+use near_contract_standards::fungible_token::{
+    metadata::{FungibleTokenMetadata, FungibleTokenMetadataProvider, FT_METADATA_SPEC},
+    FungibleToken,
+};
+use near_contract_standards::{impl_fungible_token_core, impl_fungible_token_storage};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LazyOption;
+use near_sdk::json_types::U128;
+use near_sdk::{log, near_bindgen, AccountId, Balance, BorshStorageKey, PromiseOrValue, PanicOnDefault};
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    FungibleToken,
+    Metadata,
+}
+
+#[near_bindgen]
+#[derive(BorshSerialize, BorshDeserialize, PanicOnDefault)]
+struct Contract {
+    token: FungibleToken,
+    metadata: LazyOption<FungibleTokenMetadata>,
+}
+
+#[near_bindgen]
+impl Contract {
+    #[init]
+    pub fn init(owner_id: AccountId, total_supply: U128) -> Self {
+        let metadata = FungibleTokenMetadata {
+            spec: FT_METADATA_SPEC.to_string(),
+            name: "Juice".to_string(),
+            symbol: "JUICE".to_string(),
+            icon: None,
+            reference: None,
+            reference_hash: None,
+            decimals: 0,
+        };
+
+        metadata.assert_valid();
+
+        let mut this = Self {
+            token: FungibleToken::new(StorageKey::FungibleToken),
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+        };
+
+        this.token.internal_register_account(&owner_id);
+        this.token.internal_deposit(&owner_id, total_supply.into());
+        this
+    }
+
+    fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
+        log!("Closed @{} with {}", account_id, balance);
+    }
+
+    fn on_tokens_burned(&mut self, account_id: AccountId, amount: Balance) {
+        log!("Account @{} burned {}", account_id, amount);
+    }
+}
+
+near_contract_standards::impl_fungible_token_core!(Contract, token, on_tokens_burned);
+near_contract_standards::impl_fungible_token_storage!(Contract, token, on_account_closed);
+
+#[near_bindgen]
+impl FungibleTokenMetadataProvider for Contract {
+    fn ft_metadata(&self) -> FungibleTokenMetadata {
+        self.metadata.get().unwrap()
+    }
+}
