27bf06358ddc83e29a7cf8585c2e5185e5011aef
fomotoshi <fomotoshi@ya.ru>
2021-11-28 14:19:43 +0300

    - added `bid_adding_to_state_successful` test - clean up code from warnings

--- nft_market/tests/cross_contract_simulations.rs
+++ nft_market/tests/cross_contract_simulations.rs
@@ -1,15 +1,16 @@
-use near_sdk::json_types::U128;
+use crate::utils::State;
 use near_sdk::serde_json::json;
 use near_sdk::{AccountId, Gas};
 use near_sdk_sim::{call, to_yocto, view, DEFAULT_GAS, STORAGE_AMOUNT};
-use nft_market::{SaleCondition, NO_DEPOSIT};
+use near_sdk_sim::transaction::ExecutionStatus;
+use nft_market::{OfferCondition, SaleCondition};
 use test_utils::*;
 
 mod utils;
 
 #[test]
 fn list_asks() {
-    let (root, nft, market, _alice) = utils::init();
+    let (_root, nft, market, _alice) = utils::init();
     let token_metadata = baz_token_metadata_ext();
     let token_id = "1".to_string();
     // mint 1 nft token
@@ -25,7 +26,7 @@ fn list_asks() {
         "price": "1",
     })
     .to_string();
-    // simulate frontend's call for selling nft token.
+    // simulate frontend call for selling nft token.
     call!(
         nft.user_account,
         nft.nft_approve(token_id.clone(), market.account_id(), Some(price)),
@@ -38,7 +39,7 @@ fn list_asks() {
     let sale = sale_conditions.first().unwrap();
     assert_eq!(sale.token_id, token_id);
     assert_eq!(sale.owner_id, nft.account_id());
-    assert_eq!(sale.price, U128(1));
+    assert_eq!(sale.price, 1);
 }
 
 #[test]
@@ -60,7 +61,7 @@ fn buying() {
         "price": to_yocto("10").to_string(),
     })
     .to_string();
-    // simulate frontend's call for selling nft token.
+    // simulate frontend call for selling nft token.
     call!(
         bob,
         nft.nft_approve(token_id.clone(), market.account_id(), Some(price)),
@@ -89,4 +90,65 @@ fn buying() {
     // checking that new owner is Alice
     let owner_id: AccountId = view!(nft.get_owner_by_token_id(token_id.clone())).unwrap_json();
     assert_eq!(owner_id, alice.account_id());
+}
+
+#[test]
+fn bid_adding_to_state_successful() {
+    let (root, nft, market, alice) = utils::init();
+    let token_id = "some title".to_string();
+    let token_metadata = baz_token_metadata_ext();
+    // mint 1 nft token to bob
+    let bob = root.create_user("bob".parse().unwrap(), to_yocto("100"));
+    call!(
+        nft.user_account,
+        nft.mint(token_id.clone(), token_metadata, Some(bob.account_id())),
+        deposit = (STORAGE_AMOUNT / 2)
+    )
+    .assert_success();
+    call!(alice, market.bid(token_id.clone()), deposit = 100).assert_success();
+
+    let mut conditions: Vec<OfferCondition> = view!(market.list_bids()).unwrap_json();
+    assert_eq!(conditions.len(), 1);
+
+    let condition = conditions.pop().unwrap();
+    assert_eq!(condition.token_id, token_id);
+    assert_eq!(condition.bidder_id, alice.account_id());
+    assert_eq!(condition.price, 100);
+}
+
+#[test]
+fn bid_failure_nft_token_must_refund_attached_deposit() {
+    let (root, _spoiled_nft, market) = utils::init_spoiled();
+    let alice = root.create_user("alice".parse().unwrap(), to_yocto("10"));
+    let token_id = "some_title".to_string();
+    let initial_amount = alice.get_amount();
+    let result = call!(alice, market.bid(token_id.clone()), deposit = to_yocto("5"));
+    let actual_amount = alice.get_amount();
+    let diff = initial_amount - actual_amount;
+    // the gas fee for execution tx smaller than 1, so the balance the same before and after
+    assert!(diff < to_yocto("1"));
+
+    // promise results must contain errors
+    let promise_results = result.promise_results();
+    let panic_msg = "this is spoiled `nft_token` method.";
+    let failures = promise_results
+        .clone()
+        .into_iter()
+        .filter_map(|r| match r.unwrap().status() {
+            ExecutionStatus::Failure(e) if e.to_string().contains(panic_msg) => Some(e),
+            _ => None,
+        })
+        .count();
+    assert_eq!(failures, 1);
+
+    // promise results must contain log about refund
+    let logs = promise_results
+        .into_iter()
+        .map(|r| r.unwrap().logs().to_owned())
+        .flatten()
+        .collect::<Vec<_>>();
+
+    let err_log =
+        "`nft_token` execution error was occurred, attached deposit was returned".to_string();
+    assert!(logs.contains(&err_log));
 }
--- nft_market/tests/utils.rs
+++ nft_market/tests/utils.rs
@@ -2,15 +2,17 @@ use near_sdk_sim::lazy_static_include::lazy_static_include_bytes;
 use near_sdk_sim::{deploy, init_simulator, to_yocto, ContractAccount, UserAccount};
 use nft_market::ContractContract as MarketContract;
 use nft_token::ContractContract as NFTContract;
+use spoiled_nft_token::ContractContract as SpoiledNFTContract;
 
 lazy_static_include_bytes! {
     MARKET_WASM => "./../target/wasm32-unknown-unknown/release/nft_market.wasm",
     NFT_WASM => "./../target/wasm32-unknown-unknown/release/nft_token.wasm",
+    SPOILED_NFT_WASM => "./../target/wasm32-unknown-unknown/release/spoiled_nft_token.wasm",
 }
 
-pub const ONE_YOCTO: u128 = 1;
 const MARKET_ACCOUNT_ID: &str = "market";
 const NFT_ACCOUNT_ID: &str = "nft";
+const SPOILED_NFT_ACCOUNT_ID: &str = "spoiled_nft";
 
 pub fn init() -> (
     UserAccount,
@@ -38,4 +40,42 @@ pub fn init() -> (
 
     let alice = root.create_user("alice".parse().unwrap(), to_yocto("100"));
     (root, nft_contract, market_contract, alice)
+}
+
+pub fn init_spoiled() -> (
+    UserAccount,
+    ContractAccount<SpoiledNFTContract>,
+    ContractAccount<MarketContract>,
+) {
+    let root = init_simulator(None);
+
+    let spoiled_nft_contract = deploy!(
+        contract: SpoiledNFTContract,
+        contract_id: SPOILED_NFT_ACCOUNT_ID,
+        bytes: &SPOILED_NFT_WASM,
+        signer_account: root,
+        // init_method: init(NFT_ACCOUNT_ID.parse().unwrap()),
+    );
+
+    let market_contract = deploy!(
+        contract: MarketContract,
+        contract_id: MARKET_ACCOUNT_ID,
+        bytes: &MARKET_WASM,
+        signer_account: root,
+        init_method: init(SPOILED_NFT_ACCOUNT_ID.parse().unwrap()),
+    );
+
+    (root, spoiled_nft_contract, market_contract)
+}
+
+pub trait State {
+    fn get_amount(&self) -> u128;
+}
+
+impl State for UserAccount {
+    fn get_amount(&self) -> u128 {
+        self.account()
+            .expect("account doesn't contain amount")
+            .amount
+    }
 }
