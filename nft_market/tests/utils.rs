0d4c7a126736460bdcf5df8655b85975949c724c
fomotoshi <fomotoshi@ya.ru>
2021-11-29 07:53:59 +0300

    - added new consts - added new once_cells - refactored imports

--- nft_market/tests/utils.rs
+++ nft_market/tests/utils.rs
@@ -1,8 +1,30 @@
+use near_sdk::serde_json::json;
 use near_sdk_sim::lazy_static_include::lazy_static_include_bytes;
-use near_sdk_sim::{deploy, init_simulator, to_yocto, ContractAccount, UserAccount};
+use near_sdk_sim::transaction::ExecutionStatus;
+use near_sdk_sim::{
+    call, deploy, init_simulator, to_yocto, ContractAccount, ExecutionResult, UserAccount,
+    STORAGE_AMOUNT,
+};
+use once_cell::sync::Lazy;
+
 use nft_market::ContractContract as MarketContract;
 use nft_token::ContractContract as NFTContract;
 use spoiled_nft_token::ContractContract as SpoiledNFTContract;
+use test_utils::baz_token_metadata_ext;
+
+type InitAccounts = (
+    UserAccount,
+    ContractAccount<NFTContract>,
+    ContractAccount<MarketContract>,
+    UserAccount,
+);
+
+type SpoiledInitAccounts = (
+    UserAccount,
+    ContractAccount<SpoiledNFTContract>,
+    ContractAccount<MarketContract>,
+    UserAccount,
+);
 
 lazy_static_include_bytes! {
     MARKET_WASM => "./../target/wasm32-unknown-unknown/release/nft_market.wasm",
@@ -13,6 +35,10 @@ lazy_static_include_bytes! {
 const MARKET_ACCOUNT_ID: &str = "market";
 const NFT_ACCOUNT_ID: &str = "nft";
 const SPOILED_NFT_ACCOUNT_ID: &str = "spoiled_nft";
+pub const VALID_TOKEN_ID: &str = "valid token id";
+pub const INVALID_TOKEN_ID: &str = "invalid token id";
+pub const TOKEN_PRICE: Lazy<u128> = Lazy::new(|| to_yocto("10"));
+pub const BASE_DEPOSIT: Lazy<u128> = Lazy::new(|| to_yocto("100"));
 
 pub fn init() -> (
     UserAccount,
