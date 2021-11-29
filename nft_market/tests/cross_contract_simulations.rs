65f8c8ab7b44ed510f4e039d3eaec22516df9a57
fomotoshi <fomotoshi@ya.ru>
2021-11-29 07:57:17 +0300

    - added new tests for bid

--- nft_market/tests/cross_contract_simulations.rs
+++ nft_market/tests/cross_contract_simulations.rs
@@ -110,7 +110,71 @@ fn bid_failure_nft_token_method_panic_than_must_refund_attached_deposit() {
     assert!(with_log);
 }
 
-    let err_log =
-        "`nft_token` execution error was occurred, attached deposit was returned".to_string();
-    assert!(logs.contains(&err_log));
+#[test]
+fn bid_failure_token_with_provided_id_do_not_exists_must_refund_attached_deposit() {
+    let (_root, _nft, market, alice) = utils::init_mint();
+    let initial_amount = alice.get_amount();
+    let execution_result = call!(
+        alice,
+        market.bid(INVALID_TOKEN_ID.to_string()),
+        deposit = to_yocto("5")
+    );
+    let actual_amount = alice.get_amount();
+    let diff = initial_amount - actual_amount;
+    // the gas fee for execution tx smaller than 0.1 near, so the balance the same before and after
+    assert!(diff < to_yocto("0.1"));
+    assert_eq!(execution_result.promise_errors().len(), 0);
+    let log = format!(
+        "token with id: {} doesn't exist, attached deposit was returned",
+        INVALID_TOKEN_ID
+    );
+
+    let with_log = execution_result.promise_results().contains_log(&log);
+    assert!(with_log);
 }
+
+#[test]
+fn bid_successful_and_more_than_ask_with_same_token_id_must_refund_diff() {
+    let (_root, nft, market, alice) = utils::init_mint_approve();
+    let nft_initial_amount = nft.user_account.get_amount();
+    let alice_initial_amount = alice.get_amount();
+    let double_price = *TOKEN_PRICE * 2;
+    let execution_result = call!(
+        alice,
+        market.bid(VALID_TOKEN_ID.to_string()),
+        deposit = double_price
+    );
+
+    assert_eq!(execution_result.promise_errors().len(), 0);
+
+    let alice_actual_amount = alice.get_amount();
+    let nft_actual_amount = nft.user_account.get_amount();
+
+    //actual now is less than initial
+    let alice_diff = alice_initial_amount - alice_actual_amount;
+    // the gas fee for execution tx smaller than 0.1 near, so the balance the same before and after
+    assert!(alice_diff - *TOKEN_PRICE < to_yocto("0.1"));
+    // actual now more than initial
+    let nft_diff = nft_actual_amount - nft_initial_amount;
+    assert!(nft_diff - *TOKEN_PRICE < to_yocto("0.1"));
+
+    // alice must be new owner of token and back half of attached deposit
+    let nft_token: Option<TokenExt> =
+        view!(nft.nft_token(VALID_TOKEN_ID.to_string())).unwrap_json();
+    assert_eq!(nft_token.unwrap().owner_id, alice.account_id());
+
+    let asks: Vec<SaleCondition> = view!(market.list_asks()).unwrap_json();
+    assert_eq!(asks.len(), 0);
+
+    let bids: Vec<(TokenId, Vec<OfferCondition>)> = view!(market.list_bids()).unwrap_json();
+    assert_eq!(bids.len(), 0);
+    let (token_id, offers) = bids.last().unwrap();
+    assert_eq!(token_id, VALID_TOKEN_ID);
+    assert_eq!(offers.len(), 0);
+}
+
+// #[test]
+// fn bid_successful_and_equals_to_ask_with_same_token_id() {}
+//
+// #[test]
+// fn bid_successful_and_less_than_ask_with_same_token_id_must() {}
