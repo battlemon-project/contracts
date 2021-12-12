c1e19507e311c5d45854c5715beea6b88d7e5034
fomotoshi <fomotoshi@ya.ru>
2021-12-12 10:27:31 +0300

    - added mvp

--- slots_derive_macro/src/lib.rs
+++ slots_derive_macro/src/lib.rs
@@ -1,8 +1,36 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields, FieldsNamed};
+
+#[proc_macro_derive(Slots)]
+pub fn slots(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    impl_slots(&ast)
+}
+
+fn impl_slots(ast: &DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+
+    let data_struct = match &ast.data {
+        Data::Struct(data) => data,
+        _ => panic!("works only with struct"),
+    };
+
+    let fields = match &data_struct.fields {
+        Fields::Named(FieldsNamed { named, .. }) => named.iter().flat_map(|v| &v.ident),
+        _ => panic!("works only with named fields"),
+    };
+
+    let slots = fields.filter(|v| v.to_string().contains("slot"));
+
+    let gen = quote! {
+        impl Slots for #name {
+            fn slots_id(&self) -> Vec<&TokenId> {
+                std::iter::empty()
+                    #(.chain(&self.#slots))*
+                    .collect()
+            }
+        }
+    };
+    gen.into()
 }
