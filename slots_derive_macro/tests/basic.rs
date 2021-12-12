076cbb42cc70c72e2a4a5b978288a638d1a78382
fomotoshi <fomotoshi@ya.ru>
2021-12-12 11:22:30 +0300

    - added tests for `slots_derive_macro`

--- slots_derive_macro/tests/basic.rs
+++ slots_derive_macro/tests/basic.rs
@@ -1,36 +1,61 @@
 use near_contract_standards::non_fungible_token::TokenId;
 use slots_derive_macro::Slots;
 
+/// Mocked version of original trait. It uses only for tests
+trait Slots {
+    fn slots_id(&self) -> Vec<&TokenId>;
+}
+
 #[test]
-fn get_info_from_foo() {
+fn zero_slots_to_vec() {
     #[derive(Slots)]
-    struct Foo {
+    struct Model {
+        _damage: u64,
+    }
+
+    let model = Model { _damage: 0 };
+
+    let actual = model.slots_id();
+    assert_eq!(actual, Vec::<&TokenId>::new());
+}
+
+#[test]
+fn one_slots_to_vec() {
+    #[derive(Slots)]
+    struct Model {
+        weapon_slot: Option<TokenId>,
+        _damage: u64,
+    }
+
+    let weapon = "bar".to_string();
+
+    let model = Model {
+        weapon_slot: Some(weapon.clone()),
+        _damage: 0,
+    };
+
+    let actual = model.slots_id();
+    assert_eq!(actual, vec![&weapon])
+}
+
+#[test]
+fn two_slots_to_vec() {
+    #[derive(Slots)]
+    struct Model {
         weapon_slot: Option<TokenId>,
         armor_slot: Option<TokenId>,
         _damage: u64,
     }
-
-    let weapon_slot = Some("hello".to_string());
-    let armor_slot = Some("world".to_string());
-    let ret = Foo {
+    let weapon = "bar".to_string();
+    let armor = "baz".to_string();
+    let weapon_slot = Some(weapon.clone());
+    let armor_slot = Some(armor.clone());
+    let model = Model {
         weapon_slot,
         armor_slot,
         _damage: 0,
     };
 
-    let actual = ret.slots_id();
-    assert_eq!(actual, vec![&"hello".to_string(), &"world".to_string()]);
-}
-
-#[test]
-fn get_info_from_foo_empty() {
-    #[derive(Slots)]
-    struct Foo {
-        _damage: u64,
-    }
-
-    let ret = Foo { _damage: 0 };
-
-    let actual = ret.slots_id();
-    assert_eq!(actual, Vec::<&TokenId>::new());
+    let actual = model.slots_id();
+    assert_eq!(actual, vec![&weapon, &armor]);
 }
