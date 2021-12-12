302a78814e1f2b5c90ade48ad361c3d3ce90000e
fomotoshi <fomotoshi@ya.ru>
2021-12-12 15:11:13 +0300

    - refactor token_metadata_ext

--- token_metadata_ext/src/lib.rs
+++ token_metadata_ext/src/lib.rs
@@ -1,152 +1,10 @@
 use near_contract_standards::non_fungible_token::metadata::TokenMetadata;
 use near_contract_standards::non_fungible_token::{Token, TokenId};
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::Base64VecU8;
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::AccountId;
+use nft_models::ModelKind;
 use std::collections::HashMap;
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum Option_ {
-    OnSale,
-    Auction,
-    ForRent,
-    LemonGen,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum Century {
-    Ancient,
-    OurTime,
-    Future,
-    Otherworldly,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum Type {
-    Light,
-    Medium,
-    Heavy,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum LemonGen {
-    Nakamoto,
-    Buterin,
-    Mask,
-    Jobs,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum Background {
-    Red,
-    Purple,
-    Black,
-    Yellow,
-    Green,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum Top {
-    Headdress,
-    Hairstyle,
-    Classical,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum CyberSuit {
-    Black,
-    Metallic,
-    Blue,
-    Gold,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum Expression {
-    Brooding,
-    Merry,
-    Angry,
-    Tense,
-    Relaxed,
-    Mask,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum Eyes {
-    Open,
-    Close,
-    Medium,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum Hair {
-    Elvis,
-    BobMarley,
-    Punkkez,
-    Disco,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, Copy, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
-pub enum Accessory {
-    Cigar,
-    Toothpick,
-    Tattoo,
-    Scar,
-}
-
-#[derive(
-    Serialize, Deserialize, BorshSerialize, BorshDeserialize, Copy, Clone, PartialEq, Debug,
-)]
-#[serde(crate = "near_sdk::serde")]
-pub struct TokenProperties {
-    pub option: Option_,
-    pub century: Century,
-    #[serde(rename = "type")]
-    pub type_: Type,
-    pub lemon_gen: LemonGen,
-    pub background: Background,
-    pub top: Top,
-    pub cyber_suit: CyberSuit,
-    pub expression: Expression,
-    pub eyes: Eyes,
-    pub hair: Hair,
-    pub accessory: Accessory,
-    pub winrate: Option<u8>,
-    pub rarity: u8,
-}
 
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
@@ -163,11 +21,11 @@ pub struct TokenMetadataExt {
     pub extra: Option<String>, // anything extra the NFT wants to store on-chain. Can be stringified JSON.
     pub reference: Option<String>, // URL to an off-chain JSON file with more info.
     pub reference_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
-    pub properties: TokenProperties,
+    pub model: ModelKind,
 }
 
 impl TokenMetadataExt {
-    pub fn split(self) -> (TokenMetadata, TokenProperties) {
+    pub fn split(self) -> (TokenMetadata, ModelKind) {
         let metadata = TokenMetadata {
             title: self.title,
             description: self.description,
@@ -183,7 +41,7 @@ impl TokenMetadataExt {
             reference_hash: self.reference_hash,
         };
 
-        (metadata, self.properties)
+        (metadata, self.model)
     }
 }
 
@@ -193,18 +51,18 @@ pub struct TokenExt {
     pub token_id: TokenId,
     pub owner_id: AccountId,
     pub metadata: Option<TokenMetadata>,
-    pub properties: TokenProperties,
+    pub model: ModelKind,
     pub approved_account_ids: Option<HashMap<AccountId, u64>>,
 }
 
 impl TokenExt {
-    pub fn from_parts(token: Token, properties: TokenProperties) -> TokenExt {
+    pub fn from_parts(token: Token, model: ModelKind) -> TokenExt {
         TokenExt {
             token_id: token.token_id,
             owner_id: token.owner_id,
             metadata: token.metadata,
             approved_account_ids: token.approved_account_ids,
-            properties,
+            model,
         }
     }
 }
@@ -212,13 +70,13 @@ impl TokenExt {
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use super::*;
+    use nft_models::*;
 
-    #[test]
-    fn token_metadata_ext_split() {
-        let expected_properties = TokenProperties {
+    fn get_lemon_model() -> Lemon {
+        let expected_model = Lemon {
             option: Option_::OnSale,
             century: Century::Ancient,
-            type_: Type::Light,
+            r#type: Type::Light,
             lemon_gen: LemonGen::Nakamoto,
             background: Background::Red,
             top: Top::Headdress,
@@ -229,7 +87,16 @@ mod tests {
             accessory: Accessory::Cigar,
             winrate: None,
             rarity: 0,
+            body_slot: None,
+            left_weapon_slot: None,
+            right_weapon_slot: None,
         };
+        expected_model
+    }
+
+    #[test]
+    fn token_metadata_ext_split() {
+        let expected_model = get_lemon_model();
 
         let token_metadata_ext = TokenMetadataExt {
             title: None,
@@ -244,12 +111,12 @@ mod tests {
             extra: None,
             reference: None,
             reference_hash: None,
-            properties: expected_properties,
+            model: expected_model.clone().into(),
         };
 
-        let (actual_metadata, actual_properties) = token_metadata_ext.clone().split();
+        let (actual_metadata, actual_model) = token_metadata_ext.clone().split();
 
-        assert_eq!(actual_properties, expected_properties);
+        assert_eq!(actual_model, expected_model.into());
         assert_eq!(token_metadata_ext.title, actual_metadata.title);
         assert_eq!(token_metadata_ext.description, actual_metadata.description);
         assert_eq!(token_metadata_ext.media, actual_metadata.media);
@@ -274,37 +141,14 @@ mod tests {
             approved_account_ids: None,
         };
 
-        let properties = TokenProperties {
-            option: Option_::OnSale,
-            century: Century::Ancient,
-            type_: Type::Light,
-            lemon_gen: LemonGen::Nakamoto,
-            background: Background::Red,
-            top: Top::Headdress,
-            cyber_suit: CyberSuit::Black,
-            expression: Expression::Brooding,
-            eyes: Eyes::Open,
-            hair: Hair::Elvis,
-            accessory: Accessory::Cigar,
-            winrate: None,
-            rarity: 0,
-        };
+        let expected_model = get_lemon_model();
 
-        let token_ext = TokenExt::from_parts(token.clone(), properties);
+        let token_ext = TokenExt::from_parts(token.clone(), expected_model.clone().into());
         assert_eq!(token.token_id, token_ext.token_id);
         assert_eq!(token.metadata, token_ext.metadata);
         assert_eq!(token.owner_id, token_ext.owner_id);
         assert_eq!(token.approved_account_ids, token_ext.approved_account_ids);
-        assert_eq!(properties.century, token_ext.properties.century);
-        assert_eq!(properties.accessory, token_ext.properties.accessory);
-        assert_eq!(properties.top, token_ext.properties.top);
-        assert_eq!(properties.expression, token_ext.properties.expression);
-        assert_eq!(properties.eyes, token_ext.properties.eyes);
-        assert_eq!(properties.hair, token_ext.properties.hair);
-        assert_eq!(properties.lemon_gen, token_ext.properties.lemon_gen);
-        assert_eq!(properties.option, token_ext.properties.option);
-        assert_eq!(properties.type_, token_ext.properties.type_);
-        assert_eq!(properties.rarity, token_ext.properties.rarity);
-        assert_eq!(properties.winrate, token_ext.properties.winrate);
+        let expected: ModelKind = expected_model.into();
+        assert_eq!(expected, token_ext.model)
     }
 }
