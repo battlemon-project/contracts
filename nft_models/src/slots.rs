b9484759a26e81471cff202abf90fc5605e94650
fomotoshi <fomotoshi@ya.ru>
2021-12-12 10:57:55 +0300

    - added doc to `Slots` trait

--- nft_models/src/slots.rs
+++ nft_models/src/slots.rs
@@ -1,6 +1,7 @@
 use enum_dispatch::enum_dispatch;
 use near_contract_standards::non_fungible_token::TokenId;
 
+/// This trait collect all fields that contains `slot` in the name in `Vec<&TokenId>`
 #[enum_dispatch]
 pub trait Slots {
     fn slots_id(&self) -> Vec<&TokenId>;
