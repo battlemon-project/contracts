70271eab51aec14edeb290ec1059393606bcb9ef
fomotoshi <fomotoshi@ya.ru>
2021-12-12 15:51:33 +0300

    - fixed

--- nft_models/src/lib.rs
+++ nft_models/src/lib.rs
@@ -1,14 +1,18 @@
-mod lemon;
+pub mod lemon;
 mod slots;
-mod weapon;
+pub mod weapon;
 
 use enum_dispatch::enum_dispatch;
-pub use lemon::Lemon;
+use lemon::Lemon;
 use near_contract_standards::non_fungible_token::TokenId;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::serde::{Deserialize, Serialize};
 pub use slots::Slots;
-pub use weapon::Weapon;
+use weapon::Weapon;
 
 #[enum_dispatch(Slots)]
+#[derive(Serialize, Deserialize, Clone, BorshSerialize, BorshDeserialize, Debug, PartialEq)]
+#[serde(crate = "near_sdk::serde")]
 pub enum ModelKind {
     Lemon,
     Weapon,
