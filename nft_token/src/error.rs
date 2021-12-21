32ca297acd67e5cc0e2a05b04be8ef7ff6cc81b0
fomotoshi <fomotoshi@ya.ru>
2021-12-21 20:36:55 +0300

    made errors great again!

--- nft_token/src/error.rs
+++ nft_token/src/error.rs
@@ -1,12 +1,14 @@
+use near_contract_standards::non_fungible_token::TokenId;
 use std::error::Error;
 use std::fmt::{self, Debug, Display, Formatter};
 
 pub(crate) type Result<T> = std::result::Result<T, BattlemonError>;
 
+#[derive(Clone)]
 pub enum BattlemonError {
-    ModelNotFound(String),
-    OwnerNotFound(String),
-    PutSlotError(String),
+    ModelNotFound(TokenId),
+    OwnerNotFound(TokenId),
+    InstructionError(InstructionErrorKind),
 }
 
 impl Display for BattlemonError {
@@ -18,11 +20,39 @@ impl Display for BattlemonError {
             BattlemonError::OwnerNotFound(owner_id) => {
                 write!(f, "couldn't find owner with id: {}", owner_id)
             }
-
-            BattlemonError::PutSlotError(e) => {
-                write!(f, "couldn't put slot into body because: {}", e)
+            BattlemonError::InstructionError(e) => {
+                write!(f, "provided instructions contain errors because: {}", e)
             }
         }
+    }
+}
+
+#[derive(Clone)]
+pub enum InstructionErrorKind {
+    NotEqualOwners,
+    Empty,
+    ChunkBoundsOut,
+    IncompatibleModels,
+}
+
+impl Display for InstructionErrorKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            InstructionErrorKind::NotEqualOwners => {
+                write!(f, "owners for token's models must be the same")
+            }
+            InstructionErrorKind::Empty => {
+                write!(f, "empty instructions are not allowed")
+            }
+            InstructionErrorKind::ChunkBoundsOut => write!(f, "index out of bound in chunk"),
+            InstructionErrorKind::IncompatibleModels => write!(f, "models are not compatible"),
+        }
+    }
+}
+
+impl Debug for InstructionErrorKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        <Self as Display>::fmt(self, f)
     }
 }
 
@@ -37,7 +67,7 @@ impl Error for BattlemonError {
         match self {
             BattlemonError::ModelNotFound(_) => None,
             BattlemonError::OwnerNotFound(_) => None,
-            BattlemonError::PutSlotError(_) => None,
+            BattlemonError::InstructionError(_) => None,
         }
     }
 }
