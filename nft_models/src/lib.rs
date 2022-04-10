e3d73467c41b3df623c90f498b046bea3edbc23d
ChzenChzen <fedorovdanila@me.com>
2022-04-10 21:00:26 +0800

    disable not actual code

--- nft_models/src/lib.rs
+++ nft_models/src/lib.rs
@@ -1,32 +1,22 @@
-pub mod lemon;
-pub mod manager;
-pub mod suppressor;
-pub mod weapon;
-
-use enum_dispatch::enum_dispatch;
-use lemon::Lemon;
-pub use manager::Manager;
-use near_contract_standards::non_fungible_token::TokenId;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
-use suppressor::Suppressor;
-use weapon::Weapon;
 
-#[enum_dispatch(Manager)]
+pub use lemon::Lemon;
+
+pub mod lemon;
+
 #[derive(Serialize, Deserialize, Clone, BorshSerialize, BorshDeserialize, Debug, PartialEq)]
 #[serde(crate = "near_sdk::serde", rename_all = "snake_case")]
 pub enum ModelKind {
-    Lemon,
-    Weapon,
-    Suppressor,
+    Lemon(Lemon),
 }
 
-impl ModelKind {
-    pub fn is_compatible(&self, model_kind: &Self) -> bool {
-        match (self, model_kind) {
-            (ModelKind::Lemon(_), ModelKind::Weapon(_)) => true,
-            (ModelKind::Weapon(_), ModelKind::Suppressor(_)) => true,
-            _ => false,
-        }
-    }
-}
+// impl ModelKind {
+//     pub fn is_compatible(&self, model_kind: &Self) -> bool {
+//         match (self, model_kind) {
+//             (ModelKind::Lemon(_), ModelKind::Weapon(_)) => true,
+//             (ModelKind::Weapon(_), ModelKind::Suppressor(_)) => true,
+//             _ => false,
+//         }
+//     }
+// }
