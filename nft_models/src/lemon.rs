da5211242e4e5a44f35a5b11589ddc6b6302b44e
ChzenChzen <fedorovdanila@me.com>
2022-04-10 20:55:57 +0800

    change imports

--- nft_models/src/lemon.rs
+++ nft_models/src/lemon.rs
@@ -1,9 +1,5 @@
-use crate::manager::Manager;
-use manager_derive_macro::Manager;
-use near_contract_standards::non_fungible_token::TokenId;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
-use std::collections::HashSet;
 
 #[derive(
     Serialize, Deserialize, BorshSerialize, BorshDeserialize, Clone, PartialEq, Debug, Manager,
