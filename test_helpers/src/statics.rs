c26d2908dba9103f1a44b92d3216069d20492590
ChzenChzen <fedorovdanila@me.com>
2022-06-04 23:10:34 +0800

    fix typo

--- test_helpers/src/statics.rs
+++ test_helpers/src/statics.rs
@@ -5,7 +5,7 @@ static NFT_WASM: OnceCell<Vec<u8>> = OnceCell::const_new();
 pub async fn get_nft_wasm() -> &'static [u8] {
     NFT_WASM
         .get_or_init(|| async {
-            workspaces::compile_project("../../nft-token")
+            workspaces::compile_project("../../nft_token")
                 .await
                 .expect("Failed to compile NFT token contract")
         })
