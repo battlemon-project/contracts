a8d65766a08f7c563625d78adbe4219d9e425ba4
ChzenChzen <fedorovdanila@me.com>
2022-07-03 01:43:17 +0300

    refactor tests

--- nft_token/tests/public_methods.rs
+++ nft_token/tests/public_methods.rs
@@ -1,341 +1,285 @@
-use anyhow::Context;
-use serde_json::json;
-use test_helpers::{deploy_contract, parse_near, workspaces, workspaces::prelude::*};
-const CONTRACT_WASM: &str = "../target/wasm32-unknown-unknown/release/nft_token.wasm";
+use lemotests::prelude::*;
+use lemotests_macro::add_helpers;
+use token_metadata_ext::TokenExt;
+
+const NFT_PATH: &str = "../target/wasm32-unknown-unknown/release/nft_token.wasm";
+const NFT: &str = "nft_contract";
+add_helpers!("./nft_token/nft_schema.json");
 
 #[tokio::test]
 async fn contract_is_initable() -> anyhow::Result<()> {
-    let worker = workspaces::testnet().await?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .build()
         .await?;
 
-    assert!(result.is_success());
+    let results = blockchain
+        .call_nft_contract_init(ALICE)?
+        .with_gas(Tgas(10))
+        .execute()
+        .await?;
+
+    assert!(results[0].is_success());
 
     Ok(())
 }
 
 #[tokio::test]
 async fn contract_is_initable_by_any_account() -> anyhow::Result<()> {
-    let worker = workspaces::testnet()
-        .await
-        .context("Failed to create worker")?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
-    let bob = worker
-        .dev_create_account()
-        .await
-        .context("Failed to create account for bob")?;
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .with_alice(Near(10))?
+        .build()
+        .await?;
 
-    let result = bob
-        .call(&worker, contract.id(), "init")
-        .args_json(json!({"owner_id": bob.id()}))?
-        .transact()
-        .await
-        .context("Failed to call contract's method `init`")?;
+    let alice = blockchain.alice_id()?.to_owned();
 
-    assert!(result.is_success());
+    let result = blockchain
+        .alice_call_nft_contract_init(&alice)?
+        .with_gas(Tgas(10))
+        .execute()
+        .await?;
+
+    assert!(result[0].is_success());
 
     Ok(())
 }
 
 #[tokio::test]
 async fn double_initialization_contract_rejected() -> anyhow::Result<()> {
-    let worker = workspaces::testnet()
-        .await
-        .context("Failed to create worker")?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .build()
+        .await?;
 
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
-        .await
-        .context("Failed to call `init` contract's method")?;
-    assert!(result.is_success());
-
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
+    let result = blockchain
+        .call_nft_contract_init(ALICE)?
+        .with_gas(Tgas(10))
+        .then()
+        .call_nft_contract_init(BOB)?
+        .with_gas(Tgas(10))
+        .execute()
         .await;
 
-    assert!(result
-        .unwrap_err()
-        .to_string()
-        .contains("The contract has already been initialized"));
+    let error = format!("{:?}", result.unwrap_err());
+    assert!(error.contains("The contract has already been initialized"));
 
     Ok(())
 }
 
 #[tokio::test]
 async fn mint_works() -> anyhow::Result<()> {
-    let worker = workspaces::testnet().await?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
-
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
-        .await?;
-    assert!(result.is_success());
-
-    let alice = worker.dev_create_account().await?;
-    let result = alice
-        .call(&worker, contract.id(), "nft_mint")
-        .deposit(parse_near!("1 N"))
-        .args_json(json!({"receiver_id": alice.id()}))?
-        .transact()
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .with_alice(Near(10))?
+        .build()
         .await?;
 
-    assert!(result.is_success());
+    let [nft, alice] = blockchain.string_ids()?;
+
+    blockchain
+        .call_nft_contract_init(&nft)?
+        .with_gas(Tgas(10))
+        .then()
+        .alice_call_nft_contract_nft_mint(&alice)?
+        .with_gas(Tgas(10))
+        .with_deposit(Near(1))
+        .then()
+        .view_nft_contract_nft_token("1")?
+        .execute()
+        .await?;
 
     Ok(())
 }
 
 #[tokio::test]
 async fn minted_token_belongs_to_receiver_id() -> anyhow::Result<()> {
-    let worker = workspaces::testnet().await?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .with_alice(Near(10))?
+        .build()
         .await?;
-    assert!(result.is_success());
 
-    let alice = worker.dev_create_account().await?;
-    let result = alice
-        .call(&worker, contract.id(), "nft_mint")
-        .deposit(parse_near!("1 N"))
-        .args_json(json!({"receiver_id": alice.id()}))?
-        .transact()
+    let [nft, alice] = blockchain.string_ids()?;
+
+    let result = blockchain
+        .call_nft_contract_init(&nft)?
+        .with_gas(Tgas(10))
+        .then()
+        .alice_call_nft_contract_nft_mint(&alice)?
+        .with_gas(Tgas(10))
+        .with_deposit(Near(1))
+        .then()
+        .view_nft_contract_nft_token("1")?
+        .execute()
         .await?;
-    assert!(result.is_success());
+    let view_tx = result[2].json::<TokenExt>()?;
 
-    let result = contract
-        .call(&worker, "nft_token")
-        .args_json(json!({"token_id": "1"}))?
-        .view()
-        .await?
-        .json::<token_metadata_ext::TokenExt>()?;
-    assert_eq!(result.owner_id.as_str(), alice.id().as_str());
+    assert_eq!(view_tx.token_id, "1");
+    assert_eq!(view_tx.owner_id.as_str(), alice);
 
     Ok(())
 }
 
 #[tokio::test]
 async fn nft_transfer_works() -> anyhow::Result<()> {
-    let worker = workspaces::testnet().await?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
-        .await?;
-    assert!(result.is_success());
-
-    let alice = worker.dev_create_account().await?;
-    let result = alice
-        .call(&worker, contract.id(), "nft_mint")
-        .deposit(parse_near!("1 N"))
-        .args_json(json!({"receiver_id": alice.id()}))?
-        .transact()
-        .await?;
-    assert!(result.is_success());
-
-    let bob = worker.dev_create_account().await?;
-    let result = alice
-        .call(&worker, contract.id(), "nft_transfer")
-        .deposit(1)
-        .args_json(json!({"receiver_id": bob.id(), "token_id": "1"}))?
-        .transact()
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .with_alice(Near(10))?
+        .with_bob(Near(10))?
+        .build()
         .await?;
 
-    assert!(result.is_success());
+    let [nft, alice, bob] = blockchain.string_ids()?;
 
-    let result = contract
-        .call(&worker, "nft_token")
-        .args_json(json!({"token_id": "1"}))?
-        .view()
-        .await?
-        .json::<token_metadata_ext::TokenExt>()?;
+    let result = blockchain
+        .call_nft_contract_init(&nft)?
+        .with_gas(Tgas(10))
+        .then()
+        .alice_call_nft_contract_nft_mint(&alice)?
+        .with_gas(Tgas(10))
+        .with_deposit(Near(1))
+        .then()
+        .alice_call_nft_contract_nft_transfer(&bob, "1", None, None)?
+        .with_deposit(1)
+        .with_gas(Tgas(10))
+        .then()
+        .view_nft_contract_nft_token("1")?
+        .execute()
+        .await?;
 
-    assert_eq!(result.owner_id.as_str(), bob.id().as_str());
+    let view_tx = result[3].json::<TokenExt>()?;
+    assert_eq!(view_tx.owner_id.as_str(), bob.as_str());
 
     Ok(())
 }
 
 #[tokio::test]
 async fn nft_transfer_is_prohibited_for_not_owner() -> anyhow::Result<()> {
-    let worker = workspaces::testnet().await?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .with_alice(Near(10))?
+        .with_bob(Near(10))?
+        .build()
         .await?;
-    assert!(result.is_success());
 
-    let alice = worker.dev_create_account().await?;
-    let result = alice
-        .call(&worker, contract.id(), "nft_mint")
-        .deposit(parse_near!("1 N"))
-        .args_json(json!({"receiver_id": alice.id()}))?
-        .transact()
-        .await?;
-    assert!(result.is_success());
+    let [nft, alice, bob] = blockchain.string_ids()?;
 
-    let bob = worker.dev_create_account().await?;
-    let result = bob
-        .call(&worker, contract.id(), "nft_transfer")
-        .deposit(1)
-        .args_json(json!({"receiver_id": bob.id(), "token_id": "1"}))?
-        .transact()
+    let result = blockchain
+        .call_nft_contract_init(&nft)?
+        .with_gas(Tgas(10))
+        .then()
+        .alice_call_nft_contract_nft_mint(&alice)?
+        .with_gas(Tgas(10))
+        .with_deposit(Near(1))
+        .then()
+        .bob_call_nft_contract_nft_transfer(&bob, "1", None, None)?
+        .with_deposit(1)
+        .with_gas(Tgas(10))
+        .execute()
         .await;
-    assert!(result
-        .unwrap_err()
-        .to_string()
-        .contains("Smart contract panicked: Unauthorized"));
+
+    let error = format!("{:?}", result.unwrap_err());
+    assert!(error.contains("Smart contract panicked: Unauthorized"));
 
     Ok(())
 }
 
 #[tokio::test]
 async fn transferred_token_not_allowed_for_prev_owner() -> anyhow::Result<()> {
-    let worker = workspaces::testnet().await?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .with_alice(Near(10))?
+        .with_bob(Near(10))?
+        .build()
         .await?;
-    assert!(result.is_success());
 
-    let alice = worker.dev_create_account().await?;
-    let result = alice
-        .call(&worker, contract.id(), "nft_mint")
-        .deposit(parse_near!("1 N"))
-        .args_json(json!({"receiver_id": alice.id()}))?
-        .transact()
-        .await?;
-    assert!(result.is_success());
+    let [nft, alice, bob] = blockchain.string_ids()?;
 
-    let bob = worker.dev_create_account().await?;
-    let result = alice
-        .call(&worker, contract.id(), "nft_transfer")
-        .deposit(1)
-        .args_json(json!({"receiver_id": bob.id(), "token_id": "1"}))?
-        .transact()
-        .await?;
-    assert!(result.is_success());
-
-    let result = alice
-        .call(&worker, contract.id(), "nft_transfer")
-        .deposit(1)
-        .args_json(json!({"receiver_id": alice.id(), "token_id": "1"}))?
-        .transact()
+    let result = blockchain
+        .call_nft_contract_init(&nft)?
+        .with_gas(Tgas(10))
+        .then()
+        .alice_call_nft_contract_nft_mint(&alice)?
+        .with_gas(Tgas(10))
+        .with_deposit(Near(1))
+        .then()
+        .alice_call_nft_contract_nft_transfer(&bob, "1", None, None)?
+        .with_deposit(1)
+        .with_gas(Tgas(10))
+        .then()
+        .alice_call_nft_contract_nft_transfer(&alice, "1", None, None)?
+        .with_deposit(1)
+        .with_gas(Tgas(10))
+        .execute()
         .await;
-    assert!(result
-        .unwrap_err()
-        .to_string()
-        .contains("Smart contract panicked: Unauthorized"));
+
+    let error = format!("{:?}", result.unwrap_err());
+    assert!(error.contains("Smart contract panicked: Unauthorized"));
 
     Ok(())
 }
 
 #[tokio::test]
 async fn update_token_media_works() -> anyhow::Result<()> {
-    let worker = workspaces::testnet().await?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .with_alice(Near(10))?
+        .build()
         .await?;
-    assert!(result.is_success());
 
-    let alice = worker.dev_create_account().await?;
-    let result = alice
-        .call(&worker, contract.id(), "nft_mint")
-        .deposit(parse_near!("1 N"))
-        .args_json(json!({"receiver_id": alice.id()}))?
-        .transact()
+    let [nft, alice] = blockchain.string_ids()?;
+
+    let result = blockchain
+        .call_nft_contract_init(&nft)?
+        .with_gas(Tgas(10))
+        .then()
+        .alice_call_nft_contract_nft_mint(&alice)?
+        .with_gas(Tgas(10))
+        .with_deposit(Near(1))
+        .then()
+        .call_nft_contract_update_token_media("1", "foo-hash")?
+        .with_deposit(1)
+        .with_gas(Tgas(10))
+        .then()
+        .view_nft_contract_nft_token("1")?
+        .execute()
         .await?;
-    assert!(result.is_success());
 
-    let result = contract
-        .call(&worker, "update_token_media")
-        .args_json(json!({"token_id": "1", "new_media": "foo"}))?
-        .transact()
-        .await?;
-    assert!(result.is_success());
+    let view_tx = result[3].json::<TokenExt>()?;
 
-    let result = contract
-        .call(&worker, "nft_token")
-        .args_json(json!({"token_id": "1"}))?
-        .view()
-        .await?
-        .json::<token_metadata_ext::TokenExt>()?;
-    let metadata = result.metadata.expect("Metadata must be present");
-    assert_eq!(metadata.media, Some("foo".to_string()));
+    let metadata = view_tx.metadata.expect("Metadata must be present");
+    assert_eq!(metadata.media, Some("foo-hash".to_string()));
 
     Ok(())
 }
 
 #[tokio::test]
 async fn update_token_media_can_be_called_only_by_contract_account() -> anyhow::Result<()> {
-    let worker = workspaces::testnet().await?;
-    let root = worker.dev_create_account().await?;
-    let contract =
-        deploy_contract(&worker, "nft", parse_near!("10 N"), &root, CONTRACT_WASM).await?;
-    let result = contract
-        .call(&worker, "init")
-        .args_json(json!({"owner_id": contract.id()}))?
-        .transact()
+    let blockchain = StateBuilder::sandbox()
+        .with_contract(NFT, NFT_PATH, Near(10))?
+        .with_alice(Near(10))?
+        .build()
         .await?;
-    assert!(result.is_success());
 
-    let alice = worker.dev_create_account().await?;
-    let result = alice
-        .call(&worker, contract.id(), "nft_mint")
-        .deposit(parse_near!("1 N"))
-        .args_json(json!({"receiver_id": alice.id()}))?
-        .transact()
-        .await?;
-    assert!(result.is_success());
+    let [nft, alice] = blockchain.string_ids()?;
 
-    let result = alice
-        .call(&worker, contract.id(), "update_token_media")
-        .deposit(1)
-        .args_json(json!({"token_id": "1", "new_media": "foo"}))?
-        .transact()
+    let result = blockchain
+        .call_nft_contract_init(&nft)?
+        .with_gas(Tgas(10))
+        .then()
+        .alice_call_nft_contract_nft_mint(&alice)?
+        .with_gas(Tgas(10))
+        .with_deposit(Near(1))
+        .then()
+        .alice_call_nft_contract_update_token_media("1", "foo-hash")?
+        .with_deposit(1)
+        .with_gas(Tgas(10))
+        .execute()
         .await;
 
-    assert!(result
-        .unwrap_err()
-        .to_string()
-        .contains("Smart contract panicked: Unauthorized"));
+    let error = format!("{:?}", result.unwrap_err());
+    assert!(error.contains("Smart contract panicked: Unauthorized"));
 
     Ok(())
 }
