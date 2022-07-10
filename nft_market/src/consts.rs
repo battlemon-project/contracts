9021cdaaae7f1321031b7a3eeecd0f0e12dd2a1f
ChzenChzen <fedorovdanila@me.com>
2022-07-10 22:16:23 +0300

    refactor errors and add new const

--- nft_market/src/consts.rs
+++ nft_market/src/consts.rs
@@ -1,9 +1,45 @@
+use crate::{Ask, Bid};
+use near_sdk::env::STORAGE_PRICE_PER_BYTE;
 use near_sdk::{Balance, Gas};
 
+pub const STORAGE_PER_SALE: u128 = minimum_deposit();
 pub const EVENT_PREFIX: &str = "EVENT_JSON";
 
 pub const NO_DEPOSIT: Balance = 0;
 pub const ONE_YOCTO: Balance = 1;
 pub const BUY_METHOD_TOTAL_GAS: Gas = Gas(80_000_000_000_000);
 pub const NFT_TRANSFER_GAS: Gas = Gas(44_000_000_000_000);
-pub const AFTER_NFT_TRANSFER_GAS: Gas = Gas(20_000_000_000_000); 
+pub const AFTER_NFT_TRANSFER_GAS: Gas = Gas(20_000_000_000_000);
+
+const MAX_ACCOUNT_ID_LENGTH: usize = 64;
+const MAX_TOKEN_ID_LENGTH: usize = 20; //u64::MAX in string representation is 20 chars
+
+const fn minimum_deposit() -> Balance {
+    let bid_size = std::mem::size_of::<Bid>() + MAX_ACCOUNT_ID_LENGTH + MAX_TOKEN_ID_LENGTH;
+    let ask_size = std::mem::size_of::<Ask>() + MAX_ACCOUNT_ID_LENGTH + MAX_TOKEN_ID_LENGTH;
+
+    let largest = if bid_size > ask_size {
+        bid_size
+    } else {
+        ask_size
+    };
+
+    STORAGE_PRICE_PER_BYTE * largest as u128
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn size_of_bid_equals_172() {
+        let bid_size = std::mem::size_of::<Bid>() + MAX_ACCOUNT_ID_LENGTH + MAX_TOKEN_ID_LENGTH;
+        assert_eq!(bid_size, 172);
+    }
+
+    #[test]
+    fn size_of_ask_equals_156() {
+        let ask_size = std::mem::size_of::<Ask>() + MAX_ACCOUNT_ID_LENGTH + MAX_TOKEN_ID_LENGTH;
+        assert_eq!(ask_size, 156);
+    }
+}
--- nft_market/src/error.rs
+++ nft_market/src/error.rs
@@ -1,4 +1,7 @@
-#[derive(Debug, thiserror::Error, near_sdk::FunctionError)]
+use crate::consts::STORAGE_PER_SALE;
+use std::fmt::{Debug, Display, Formatter};
+
+#[derive(thiserror::Error, near_sdk::FunctionError)]
 pub enum ContractError {
     #[error("Contract's account id which call market's method are not supported")]
     NotAuthorized,
@@ -6,4 +9,12 @@ pub enum ContractError {
     BidError(String),
     #[error(transparent)]
     SerdeError(#[from] near_sdk::serde_json::Error),
+    #[error("Requires minimum deposit of {STORAGE_PER_SALE}")]
+    InsufficientDeposit,
+}
+
+impl Debug for ContractError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        <Self as Display>::fmt(self, f)
+    }
 }
