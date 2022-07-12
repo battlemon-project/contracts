ac1c3bbfbf5efbb03430959f33e0b72671ac450a
ChzenChzen <fedorovdanila@me.com>
2022-07-12 15:16:33 +0300

    add new helpers

--- nft_market/src/helpers.rs
+++ nft_market/src/helpers.rs
@@ -1,7 +1,31 @@
-use near_sdk::AccountId;
+use crate::{ContractError, STORAGE_PER_SALE};
+use near_sdk::{AccountId, env};
 
 impl crate::Contract {
     pub(crate) fn total_orders_by_id(&self, id: &AccountId) -> usize {
         self.count_bids_for_account(id) + self.count_asks_for_account(id)
     }
+
+    pub(crate) fn check_storage_deposits(&self, id: &AccountId) -> Result<(), ContractError> {
+        let paid_storage = self.storage_deposits.get(id).copied().unwrap_or_default();
+
+        let required_storage = (self.total_orders_by_id(id) + 1) as u128 * STORAGE_PER_SALE;
+
+        if paid_storage < required_storage {
+            return Err(ContractError::StorageError(
+                "Not enough storage deposits to create new order",
+            ));
+        }
+
+        Ok(())
+    }
+}
+
+pub fn check_cross_contract_call(id: &AccountId) -> Result<(), ContractError> {
+    if env::predecessor_account_id() != *id {
+        return Err(ContractError::NotAuthorized(
+            "The predecessor account id isn't allowed",
+        ));
+    }
+    Ok(())
 }
