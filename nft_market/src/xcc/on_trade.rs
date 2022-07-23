6cca6ec8c00e36eacb1dbb0d831c8cd5ceb4c0ea
ChzenChzen <fedorovdanila@me.com>
2022-07-23 18:23:15 +0300

    fix event emitting

--- nft_market/src/xcc/on_trade.rs
+++ nft_market/src/xcc/on_trade.rs
@@ -1,5 +1,7 @@
-use crate::{Ask, Bid, Contract, ContractExt};
-use near_sdk::{near_bindgen, Promise, PromiseError};
+use crate::{consts, Ask, Bid, Contract, ContractExt};
+use consts::EVENT_PREFIX;
+use near_sdk::json_types::U128;
+use near_sdk::{env, near_bindgen, Promise, PromiseError};
 
 #[near_bindgen]
 impl Contract {
@@ -13,14 +15,24 @@ impl Contract {
     ) {
         trade.unwrap();
         let to_asker = Promise::new(ask.account_id().to_owned());
+        let trade_price;
         if change {
-            let diff = bid.price() - ask.price();
+            trade_price = ask.price();
+            let diff = bid.price() - trade_price;
             let to_bidder = Promise::new(bid.account_id().to_owned()).transfer(diff);
-            to_asker.transfer(ask.price()).and(to_bidder);
+            to_asker.transfer(trade_price).and(to_bidder);
         } else {
-            to_asker.transfer(bid.price());
+            trade_price = bid.price();
+            to_asker.transfer(trade_price);
         }
 
         self.clean_ask_and_bid(&bid);
+        let json = near_sdk::serde_json::json!({
+           "prev_owner": ask.account_id(),
+           "curr_owner": bid.account_id(),
+           "token_id": ask.token_id(),
+           "price": U128(trade_price),
+        });
+        env::log_str(&format!("{EVENT_PREFIX}:{json}"));
     }
 }
