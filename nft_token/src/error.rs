8e611958162dc91758473881257b4c7f7dd2588e
ChzenChzen <fedorovdanila@me.com>
2022-08-30 20:32:36 +0300

    refactor errors

--- nft_token/src/error.rs
+++ nft_token/src/error.rs
@@ -1,73 +1,17 @@
 use near_contract_standards::non_fungible_token::TokenId;
-use std::error::Error;
-use std::fmt::{self, Debug, Display, Formatter};
 
-pub(crate) type Result<T> = std::result::Result<T, BattlemonError>;
-
-#[derive(Clone)]
-pub enum BattlemonError {
+#[derive(Debug, thiserror::Error, near_sdk::FunctionError)]
+pub enum ContractError {
+    #[error("Failed to authorize contract call: {0}")]
+    NotAuthorized(&'static str),
+    #[error("Failed to find model for token with id: {0}")]
     ModelNotFound(TokenId),
+    #[error("Failed to find owner for token with id: {0}")]
     OwnerNotFound(TokenId),
-    InstructionError(InstructionErrorKind),
+    #[error("Provided instructions contain errors because: {0}")]
+    InstructionError(String),
+    #[error(transparent)]
+    SerdeError(#[from] near_sdk::serde_json::Error),
 }
 
-impl Display for BattlemonError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            BattlemonError::ModelNotFound(token_id) => {
-                write!(f, "couldn't find the model for token with id: {}", token_id)
-            }
-            BattlemonError::OwnerNotFound(owner_id) => {
-                write!(f, "couldn't find owner with id: {}", owner_id)
-            }
-            BattlemonError::InstructionError(e) => {
-                write!(f, "provided instructions contain errors because: {}", e)
-            }
-        }
-    }
-}
-
-#[derive(Clone)]
-pub enum InstructionErrorKind {
-    NotEqualOwners,
-    Empty,
-    ChunkBoundsOut,
-    IncompatibleModels,
-}
-
-impl Display for InstructionErrorKind {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            InstructionErrorKind::NotEqualOwners => {
-                write!(f, "owners for token's models must be the same")
-            }
-            InstructionErrorKind::Empty => {
-                write!(f, "empty instructions are not allowed")
-            }
-            InstructionErrorKind::ChunkBoundsOut => write!(f, "index out of bound in chunk"),
-            InstructionErrorKind::IncompatibleModels => write!(f, "models are not compatible"),
-        }
-    }
-}
-
-impl Debug for InstructionErrorKind {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        <Self as Display>::fmt(self, f)
-    }
-}
-
-impl Debug for BattlemonError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        <Self as Display>::fmt(self, f)
-    }
-}
-
-impl Error for BattlemonError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match self {
-            BattlemonError::ModelNotFound(_) => None,
-            BattlemonError::OwnerNotFound(_) => None,
-            BattlemonError::InstructionError(_) => None,
-        }
-    }
-}
+pub(crate) type Result<T> = std::result::Result<T, ContractError>;
