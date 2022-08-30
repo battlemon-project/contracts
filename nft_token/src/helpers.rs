fd1406ddf345eebba919f8b8a6f2c05d85db564f
ChzenChzen <fedorovdanila@me.com>
2022-08-30 20:33:23 +0300

    add helper for burning

--- nft_token/src/helpers.rs
+++ nft_token/src/helpers.rs
@@ -1,23 +1,58 @@
 use near_contract_standards::non_fungible_token::TokenId;
-use near_sdk::{env, AccountId};
-use std::fmt::Debug;
+use near_sdk::AccountId;
 
 use battlemon_models::nft::ModelKind;
 
-use crate::error::{BattlemonError as BtlError, Result};
+use crate::error::{ContractError, Result};
 use crate::Contract;
 
 impl Contract {
-    pub(crate) fn owner(&self, body_id: &TokenId) -> Result<AccountId> {
+    pub(crate) fn owner(&self, id: &TokenId) -> Result<AccountId> {
         self.tokens
             .owner_by_id
-            .get(&body_id)
-            .ok_or_else(|| BtlError::OwnerNotFound(body_id.to_owned()))
+            .get(id)
+            .ok_or_else(|| ContractError::OwnerNotFound(id.to_owned()))
     }
 
-    pub(crate) fn model(&self, token_id: &TokenId) -> Result<ModelKind> {
+    pub(crate) fn model(&self, id: &TokenId) -> Result<ModelKind> {
         self.model_by_id
-            .get(token_id)
-            .ok_or_else(|| BtlError::ModelNotFound(token_id.to_owned()))
+            .get(id)
+            .ok_or_else(|| ContractError::ModelNotFound(id.to_owned()))
+    }
+
+    pub(crate) fn burn_token(&mut self, token_id: &TokenId) {
+        self.model_by_id.remove(token_id);
+        let tokens = &mut self.tokens;
+        let owner_id = tokens.owner_by_id.remove(token_id).unwrap();
+
+        if let Some(collection) = &mut tokens.token_metadata_by_id {
+            collection.remove(token_id);
+        }
+
+        if let Some(collection) = &mut tokens.tokens_per_owner {
+            let mut tokens = collection.get(&owner_id).unwrap();
+            tokens.remove(token_id);
+            if tokens.is_empty() {
+                collection.remove(&owner_id);
+            } else {
+                collection.insert(&owner_id, &tokens);
+            }
+        }
+
+        if let Some(collection) = &mut tokens.approvals_by_id {
+            collection.remove(token_id);
+        }
+
+        if let Some(collection) = &mut tokens.next_approval_id_by_id {
+            collection.remove(token_id);
+        }
+
+        near_contract_standards::non_fungible_token::events::NftBurn {
+            owner_id: &owner_id,
+            token_ids: &[token_id],
+            authorized_id: None,
+            memo: None,
+        }
+        .emit();
     }
 }
